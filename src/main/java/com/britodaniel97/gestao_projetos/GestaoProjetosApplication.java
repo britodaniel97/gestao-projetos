09e6737fa2f457fb5489dc8a178c97e2dbffdd76
Daniel Brito <daniel.brito@to-brasil.com>
2025-02-22 17:42:22 -0300

    primeiro commit

---
+++ src/main/java/com/britodaniel97/gestao_projetos/GestaoProjetosApplication.java
@@ -0,0 +1,13 @@
+package com.britodaniel97.gestao_projetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoProjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoProjetosApplication.class, args);
+		System.out.println("oi");
+	}
+}
